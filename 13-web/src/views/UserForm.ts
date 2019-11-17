ecd5535a6c1d8b674d9dd3ac2baa46e12b773ff8
ASAP <alexander.sapountzis@gmail.com>
2019-11-17 18:09:19 -0500

    Project 13 - Add simple views

---
+++ 13-web/src/views/UserForm.ts
@@ -0,0 +1,49 @@
+export class UserForm {
+  constructor(public parent: Element) {}
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('ouch');
+  }
+
+  onHeaderHover(): void {
+    console.log('hover');
+  }
+
+  template(): string {
+    return `
+      <div>
+        <h1>User Form</h1>
+        <input />
+        <button>Click Me</button>
+      </div>
+    `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+
+  render() {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
+
+    this.parent.append(templateElement.content);
+  }
+}
