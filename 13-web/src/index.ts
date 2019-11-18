e2a50473b8638ec0edf074395c48ae22a01a94c4
ASAP <alexander.sapountzis@gmail.com>
2019-11-17 19:32:57 -0500

    Project 13 - Nest Views

--- 13-web/src/index.ts
+++ 13-web/src/index.ts
@@ -1,4 +1,4 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'Jim', age: 42 });
@@ -6,8 +6,9 @@ const user = User.buildUser({ name: 'Jim', age: 42 });
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(root, user);
-  userForm.render();
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+  console.log('user edit', userEdit);
 } else {
   throw new Error('Root Element not found');
 }
---
+++ 13-web/src/views/UserEdit.ts
@@ -0,0 +1,27 @@
+import { View } from './View';
+import { User, UserProps } from '../models/User';
+import { UserForm } from './UserForm';
+import { UserShow } from './UserShow';
+
+export class UserEdit extends View<User, UserProps> {
+  regionsMap(): { [key: string]: string } {
+    return {
+      userShow: '.user-show',
+      userForm: '.user-form'
+    };
+  }
+
+  template(): string {
+    return `
+      <div>
+        <div class="user-show"></div>
+        <div class="user-form"></div>
+      </div>
+    `;
+  }
+
+  onRender() {
+    new UserShow(this.regions.userShow, this.model).render();
+    new UserForm(this.regions.userForm, this.model).render();
+  }
+}
